6504b9e90faad9f0ca67a0ee39f702ec4a98307d
Francus13 <francis.rinaldi@gmail.com>
2026-02-27 09:47:58 -0500

    gitignore

--- val-coval/.Makefile.rocq.d
+++
@@ -1,2 +0,0 @@
-SynSem.vo SynSem.glob SynSem.v.beautified SynSem.required_vo: SynSem.v /home/francus/.opam/default/lib/rocq-runtime/rocqworker
-SynSem.vos SynSem.vok SynSem.required_vos: SynSem.v /home/francus/.opam/default/lib/rocq-runtime/rocqworker
